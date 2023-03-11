3d890e09abd0dfecb5b1e64bbfe4675b42172b66
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-03-11 19:22:23 +0400

    Updating node

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// CORS
+app.use(cors());
+
+// Get acces to req body
+app.use(express.json());
+
+// SERVING IMAGES
+app.use(express.static('../public'));
+
+// UNHANDLED ROUTES
+app.use('/', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'No resuorce found!',
+  });
 });
 
-export default app
+export default app;
